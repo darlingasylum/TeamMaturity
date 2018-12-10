52ed4063796dd86ed786c9ddc75b2fb69bf19a84
julien.conan <julien.conan@PARLP127.leboncoin.loc>
2018-12-10 12:14:29 +0100

    Fermeture propre des connexions sql

--- database.js
+++ database.js
@@ -7,19 +7,20 @@ const connection = mysql.createConnection({
   database: "maturity"
 });
 
-connection.connect(function(err) {});
 
-const end = () => {
-  connection.end();
-};
 
 // HOME : RECUPERE LE NOM DES FT
 const getTeam = clbk => {
+  connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   connection.query("SELECT * FROM feature_team", function(
     error,
     results,
     fields
   ) {
+    connection.end();
     if (error) return clbk(error, null);
     return clbk(null, results);
   });
@@ -27,10 +28,14 @@ const getTeam = clbk => {
 
 // BOARD : RECUPERE LES CAMPAGNES EN COURS OU PASSEES
 const getCampaigns = (id_ft, clbk) => {
+  connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   connection.query(
     `SELECT * FROM campagnes WHERE id_ft_camp=${id_ft}`,
     function(error, results, fields) {
-      console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -39,10 +44,14 @@ const getCampaigns = (id_ft, clbk) => {
 
 // BOARD : RECUPERE LE NOM DE LA FT
 const getFTName = (id_ft, clbk) => {
+   connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   connection.query(
     `SELECT nom_ft FROM feature_team WHERE id_ft=${id_ft}`,
     function(error, results, fields) {
-      console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -51,13 +60,15 @@ const getFTName = (id_ft, clbk) => {
 
 //BOARD DIALOG : POSTE LE NOM D'UNE NOUVELLE CAMPAGNE
 const postCampaignName = (clbk, data) => {
+   connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   const q = "INSERT INTO campagnes(nom_camp, id_ft_camp) VALUES (?, ?)";
   const payload = [data.nom_campagne, data.id_ft];
   //console.log(payload);
   connection.query(q, payload, function(error, results, cols) {
-    // console.log(error);
-    // console.log(results);
-    // console.log(cols);
+    connection.end();
     if (error) return clbk(error, null);
     return clbk(null, results);
   });
@@ -65,12 +76,16 @@ const postCampaignName = (clbk, data) => {
 
 //CAMPAIGN : RECUPERE LES QUESTIONS "PROCESS"
 const getProcessQuestions = (currentCampaignId, clbk) => {
+  connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   //console.log("je suis dans DB questions");
   connection.query(
     `SELECT * FROM questions LEFT JOIN resultats ON questions.id_q = resultats.id_q_r AND id_camp_r = ${currentCampaignId} WHERE chapitre_q LIKE 'Process%'`,
     //et envoyer les réponses qui existent déjà. s
     function(error, results, fields) {
-      // console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -79,11 +94,14 @@ const getProcessQuestions = (currentCampaignId, clbk) => {
 
 //CAMPAIGN : RECUPERE LES QUESTIONS "QUALITE"
 const getQualityQuestions = (currentCampaignId, clbk) => {
-  console.log(currentCampaignId);
+    connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   connection.query(
     `SELECT * FROM questions LEFT JOIN resultats ON questions.id_q = resultats.id_q_r AND id_camp_r = ${currentCampaignId} WHERE chapitre_q = 'Qualité'`,
     function(error, results, fields) {
-      //console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -92,9 +110,14 @@ const getQualityQuestions = (currentCampaignId, clbk) => {
 
 //CAMPAIGN : RECUPERE LES QUESTIONS "VALEUR"
 const getValueQuestions = (currentCampaignId, clbk) => {
+    connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   connection.query(
     `SELECT * FROM questions LEFT JOIN resultats ON questions.id_q = resultats.id_q_r AND id_camp_r = ${currentCampaignId} WHERE chapitre_q LIKE 'Valeur%'`,
     function(error, results, fields) {
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -103,11 +126,15 @@ const getValueQuestions = (currentCampaignId, clbk) => {
 
 //  THEMES : RECUPERE L'ID DE LA CAMPAGNE (nécessaire au POST des réponses)
 const getCampaignId = (id_ft, campaign_name, clbk) => {
+    connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   // console.log(campaign_name, id_ft);
   connection.query(
     `SELECT * FROM campagnes WHERE id_ft_camp=${id_ft} AND nom_camp='${campaign_name}'`,
     function(error, results, fields) {
-      //console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -116,13 +143,17 @@ const getCampaignId = (id_ft, campaign_name, clbk) => {
 
 // QUESTIONDIALOG : RECUPERE L'INTITULE D'UNE QUESTION
 const getQuestion = (id_q, clbk) => {
+    connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   // console.log("je suis dans DB questions");
   connection.query(`SELECT * FROM questions WHERE id_q=${id_q}`, function(
     error,
     results,
     fields
   ) {
-    // console.log(results);
+    connection.end();
     if (error) return clbk(error, null);
     return clbk(null, results);
   });
@@ -130,13 +161,16 @@ const getQuestion = (id_q, clbk) => {
 
 // QUESTIONDIALOG : POST LES RESULTATS DES QUESTIONS OU UPDATE SI UNE REPONSE EXISTE DEJA
 const send_response = (clbk, data) => {
+   connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   const payload = [
     data.id_camp_r,
     data.id_q_r,
     data.reponse_r,
     data.commentaire_r
   ];
-  console.log(payload);
   connection.query(
     `SELECT * FROM resultats WHERE id_camp_r = ${data.id_camp_r} AND id_q_r = ${
       data.id_q_r
@@ -151,9 +185,7 @@ const send_response = (clbk, data) => {
         }' WHERE id_camp_r = ${data.id_camp_r} AND id_q_r = ${data.id_q_r}`;
 
         connection.query(sql_update, payload, function(error, results, cols) {
-          // console.log(error);
-          // console.log(results);
-          // console.log(cols);
+          connection.end();
           if (error) return clbk(error, null);
           return clbk(null, results);
         });
@@ -162,9 +194,7 @@ const send_response = (clbk, data) => {
           "INSERT INTO resultats(id_camp_r, id_q_r, reponse_r, commentaire_r) VALUES (?, ?, ?, ?)";
 
         connection.query(sql, payload, function(error, results, cols) {
-          console.log(error);
-          console.log(results);
-          // console.log(cols);
+          connection.end();
           if (error) return clbk(error, null);
           return clbk(null, results);
         });
@@ -175,15 +205,17 @@ const send_response = (clbk, data) => {
 
 // THEMES : CHANGE LE STATUT DE LA CAMPAGNE AU CLIC SUR VALIDER
 const changeStatusCampaign = (clbk, data) => {
+    connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   const q = `UPDATE campagnes SET statut_camp=1 WHERE id_ft_camp=${
     data.id_ft
   } AND id_camp=${data.id_camp}`;
   const payload = [data.id_ft, data.id_camp];
   //console.log(payload);
   connection.query(q, payload, function(error, results, cols) {
-    // console.log(error);
-    // console.log(results);
-    // console.log(cols);
+   connection.end();
     if (error) return clbk(error, null);
     return clbk(null, results);
   });
@@ -191,12 +223,16 @@ const changeStatusCampaign = (clbk, data) => {
 
 //RESULTS : RECUPERE LES ID DES 3 CAMPAGNES N, N-1, N-2
 const get3campaigns = (id_ft, currentCampaignId, clbk) => {
+    connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
   //console.log(campaign_name, id_ft);
   connection.query(
     //`SELECT * FROM questions LEFT JOIN resultats ON questions.id_q = resultats.id_q_r AND id_camp_r = ${currentCampaignId} WHERE chapitre_q LIKE 'Process%'`,
     `SELECT * FROM campagnes WHERE id_ft_camp=${id_ft} AND id_camp BETWEEN 1 AND ${currentCampaignId} ORDER BY id_camp DESC LIMIT 3`,
     function(error, results, fields) {
-      // console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -205,11 +241,15 @@ const get3campaigns = (id_ft, currentCampaignId, clbk) => {
 
 //RESULTS : RECUPERE LES RESULTATS DE LA CAMPAGNE N
 const getResultsN = (currentCampaignId, clbk) => {
-  // console.log(campaign_name, id_ft);
+   connection.connect(function(err) 
+  {
+      return clbk(error, null);
+  });
+
   connection.query(
     `SELECT * FROM questions LEFT JOIN resultats ON questions.id_q = resultats.id_q_r AND id_camp_r = ${currentCampaignId}`,
     function(error, results, fields) {
-      //console.log(results);
+      connection.end();
       if (error) return clbk(error, null);
       return clbk(null, results);
     }
@@ -231,5 +271,4 @@ module.exports = {
   get3campaigns,
   getResultsN,
 
-  end
 };
